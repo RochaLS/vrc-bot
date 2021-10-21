b55be8c96f27bce3a2e537fa18d6fa2402a16465
Lucas Rocha <ld.rocha@hotmail.com>
2021-10-20 18:05:29 -0700

    create deploy commands and listen to command interaction

---
+++ deploy-commands.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const commands = [];
+
+const pingPong = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
+commands.push(pingPong);
+
+commands.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+
+});
